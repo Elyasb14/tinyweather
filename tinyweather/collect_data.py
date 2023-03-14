37399c15d1d73786e94a2f19ffade150c77e6ced
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2023-03-13 22:00:14 -0700

    commit

--- tinyweather/collect_data.py
+++ tinyweather/collect_data.py
@@ -1,23 +1,26 @@
 from sensors import Rg15, Bme280
 import argparse
-import time
+import pandas as pd
+import os
 
 # adds cli flags
 parser = argparse.ArgumentParser()
 parser.add_argument("--device", default="/dev/tty.usbserial-0001", help="serial device to use")
 parser.add_argument("--reset", type=bool, help="reset all sensors that have the ability to be reset")
+parser.add_argument("--clean", type=bool, default=False, help="cleans data")
+
 args = parser.parse_args()
 
 
-env = Bme280()
-rain = Rg15("/dev/ttyUSB0")
+# env = Bme280()
+rain = Rg15(args.device)
 
 
 def main():
     rain.save_data(rain.parse_data())
-    env.parse_data()
-    time.sleep(1)
-    env.save_data(env.parse_data())
+    # # env.parse_data()
+    # time.sleep(1)
+    # env.save_data(env.parse_data())
 
 if __name__ == "__main__":
     main()
