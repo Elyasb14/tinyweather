4681dc038cc3ae0a7a105e96adac06d6f94cd537
Elyas Bianchi <elyasb99@gmail.com>
2024-08-27 15:49:19 -0700

    trying for a zig tcp server

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,18 @@
 const std = @import("std");
 const net = std.net;
 
-pub fn main() !void {
-    std.debug.print("hello, world", .{});
+const ArenaAllocator = std.heap.ArenaAllocator;
+
+pub fn main() anyerror!void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    // const allocator = gpa.allocator() gc;
+    
+    const address = try net.Address.parseIp("127.0.0.1", 5501);
+    var listener = try address.listen(.{
+        .reuse_address = true,
+        .kernel_backlog = 1024,
+    });
+    defer listener.deinit();
+    std.log.info("listening at {any}\n", .{address});
+
 }
