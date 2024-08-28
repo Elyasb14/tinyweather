c90a30d46599134adadabe83438ce8537a49ce4d
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2024-08-27 22:07:27 -0700

    tcp server tinkering

--- src/main.zig
+++ src/main.zig
@@ -1,18 +1,41 @@
 const std = @import("std");
 const net = std.net;
+const print = std.debug.print;
 
-const ArenaAllocator = std.heap.ArenaAllocator;
+const Packet = struct {
+    version: u8,
+    data: []u8,
+};
 
-pub fn main() anyerror!void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    // const allocator = gpa.allocator() gc;
-    
-    const address = try net.Address.parseIp("127.0.0.1", 5501);
-    var listener = try address.listen(.{
-        .reuse_address = true,
-        .kernel_backlog = 1024,
-    });
-    defer listener.deinit();
-    std.log.info("listening at {any}\n", .{address});
+pub fn main() !void {
+    var gpa_alloc = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(gpa_alloc.deinit() == .ok);
+    const gpa = gpa_alloc.allocator();
 
+    const addr = std.net.Address.initIp4(.{ 0, 0, 0, 0 }, 3667);
+    var server = try addr.listen(.{});
+
+    std.log.info("Server listening on port 3667", .{});
+
+    var client = try server.accept();
+    defer client.stream.close();
+
+    const client_reader = client.stream.reader();
+    const client_writer = client.stream.writer();
+    while (true) {
+        //this is []u8
+        const msg = try client_reader.readUntilDelimiterOrEofAlloc(gpa, '\n', 65536) orelse break;
+        defer gpa.free(msg);
+
+        print("{c}\n", .{msg});
+        print("{c}\n", .{msg[0]});
+        try client_writer.writeAll(msg);
+    }
+}
+
+test "simple test" {
+    var list = std.ArrayList(i32).init(std.testing.allocator);
+    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+    try list.append(42);
+    try std.testing.expectEqual(@as(i32, 42), list.pop());
 }
