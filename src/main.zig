c7f1e10c72d9eaa3b826b16f9363c717cf95e340
Elyas Bianchi <elyasb99@gmail.com>
2024-09-05 17:01:17 -0700

    not sure how i wanna do this tcp protocol

--- src/main.zig
+++ src/main.zig
@@ -1,34 +1,34 @@
 const std = @import("std");
 const net = std.net;
 const print = std.debug.print;
+const assert = std.debug.assert;
 
 const version = 1;
 
 const Packet = struct {
     version: u8,
     is_request: bool,
+    len: usize,
     data: []const u8,
 
     const Self = @This();
 
-    pub fn new(data: []u8, is_request: bool) Packet {
-        return Packet{ .version = version, .data = data, .is_request = is_request };
+    pub fn new(data: []u8) Packet {
+        return data;
     }
 
     pub fn encode(self: Self, buf: []u8) []u8 {
-        std.debug.assert(self.version == 1);
-
         buf[0] = self.version;
         buf[1] = @intFromBool(self.is_request);
         // use @memcpy here to copy self.data into buf
-        @memcpy(buf[2..], self.data);
+        @memcpy(buf[2..][0..self.data.len], self.data);
         return buf;
     }
 };
 
 pub fn main() !void {
     var gpa_alloc = std.heap.GeneralPurposeAllocator(.{}){};
-    defer std.debug.assert(gpa_alloc.deinit() == .ok);
+    defer assert(gpa_alloc.deinit() == .ok);
     const gpa = gpa_alloc.allocator();
 
     const addr = std.net.Address.initIp4(.{ 0, 0, 0, 0 }, 3667);
@@ -46,13 +46,14 @@ pub fn main() !void {
         const msg = try client_reader.readUntilDelimiterOrEofAlloc(gpa, '\n', 65536) orelse break;
         defer gpa.free(msg);
 
-        // for (packet) |byte| {
-        //     print("{c}\n", .{byte});
-        // }
+        const packet = Packet.new(msg);
+        var buf: [1024]u8 = undefined;
+        const encoded = packet.encode(&buf);
+        print("{c}", .{encoded});
     }
 }
 
-test "simple test" {
+test "test encode" {
     var list = std.ArrayList(i32).init(std.testing.allocator);
     defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
     try list.append(42);
