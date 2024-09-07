6631f3740a35cafd8d5343558e605269acb29b94
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2024-09-07 14:50:38 -0700

    my encoding and decoding pipeline is not working

--- src/packet.zig
+++ src/packet.zig
@@ -17,7 +17,7 @@ pub const Packet = struct {
     pub fn encode(self: Self, buf: []u8) []u8 {
         buf[0] = self.version;
         @memcpy(buf[1..][0..self.len], self.data);
-        return buf[0..self.len];
+        return buf[0 .. self.len + 1];
     }
 
     // takes encoded buffer ([]u8), constructs a packet
