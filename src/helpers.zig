2baf308e72c20582c63cc2466cb194afbebf9bb7
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2024-09-22 16:04:04 -0700

    helpers

--- src/helpers.zig
+++ src/helpers.zig
@@ -1,3 +1,14 @@
 const std = @import("std");
 
-test "u8 to f32 and back" {}
+pub fn f32_to_bytes(val: f32) *const [4]u8 {
+    const bytes = std.mem.toBytes(val);
+    std.log.info("val: {any}, bytes: {any}", .{ val, bytes });
+    return &bytes;
+}
+
+const testing = std.testing;
+
+test "u8 to f32 and back" {
+    const val: f32 = 10.3;
+    const bytes = f32_to_bytes(val);
+}
