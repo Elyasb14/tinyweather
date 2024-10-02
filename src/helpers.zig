48c17708a18b6aa8f8c2e24254de940940c3a48b
Elyas Bianchi <elyasb99@gmail.com>
2024-10-02 16:22:57 -0700

    helpers for bytearray to f32 cast and back and test

--- src/helpers.zig
+++ src/helpers.zig
@@ -12,8 +12,32 @@ pub fn color_string(str: []const u8, color: Colors, allocator: Allocator) ![]con
         },
     }
 }
-pub fn f32_to_bytes(val: f32) *const [4]u8 {
-    const bytes = std.mem.toBytes(val);
-    std.log.info("val: {any}, bytes: {any}", .{ val, bytes });
-    return &bytes;
+
+// Function to convert f32 to a byte array
+pub fn f32_to_bytes(value: f32) [4]u8 {
+    return std.mem.toBytes(value);
+}
+
+// Function to convert a byte array back to f32
+pub fn bytes_to_f32(bytes: [4]u8) f32 {
+    return std.mem.bytesToValue(f32, &bytes);
+}
+
+const testing = std.testing;
+
+test "convert f32 to bytes and back" {
+    const val1 = 10.32;
+    const val2 = 10.22223435;
+    const val3 = -1000.32;
+    const val4 = 1000000.33;
+
+    const bytes1 = f32_to_bytes(val1);
+    const bytes2 = f32_to_bytes(val2);
+    const bytes3 = f32_to_bytes(val3);
+    const bytes4 = f32_to_bytes(val4);
+
+    try testing.expectEqual(bytes_to_f32(bytes1), val1);
+    try testing.expectEqual(bytes_to_f32(bytes2), val2);
+    try testing.expectEqual(bytes_to_f32(bytes3), val3);
+    try testing.expectEqual(bytes_to_f32(bytes4), val4);
 }
