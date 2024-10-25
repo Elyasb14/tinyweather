1ef71dfd3ff8b424bbbc9e61c5ac09781f68f9e1
Elyas Bianchi <elyasb99@gmail.com>
2024-10-25 14:05:56 -0700

    catch the error and do something with it

--- src/client.zig
+++ src/client.zig
@@ -5,7 +5,10 @@ const tcp = @import("tcp.zig");
 
 pub fn main() !void {
     const address = try net.Address.parseIp4("127.0.0.1", 8080);
-    const stream = try net.tcpConnectToAddress(address);
+    const stream = net.tcpConnectToAddress(address) catch |err| {
+        std.log.err("Can't connect to address: {any}... error: {any}", .{ address, err });
+        return;
+    };
     defer stream.close();
 
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
