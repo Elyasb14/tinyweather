2399bc50168790944e28726dc99ad7893eb03972
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2024-10-31 21:33:13 -0700

    build system changes

--- build.zig
+++ build.zig
@@ -18,20 +18,8 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    const server_run_cmd = b.addRunArtifact(server_exe);
-    const client_run_cmd = b.addRunArtifact(client_exe);
-    server_run_cmd.step.dependOn(b.getInstallStep());
-    client_run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        server_run_cmd.addArgs(args);
-        client_run_cmd.addArgs(args);
-    }
-
-    const server_run_step = b.step("run-server", "run server");
-    const client_run_step = b.step("run-client", "run client");
-    server_run_step.dependOn(&server_run_cmd.step);
-    client_run_step.dependOn(&client_run_cmd.step);
+    b.installArtifact(server_exe);
+    b.installArtifact(client_exe);
 
     const server_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/server.zig"),
@@ -46,8 +34,8 @@ pub fn build(b: *std.Build) void {
     });
 
     const run_exe_unit_tests = b.addRunArtifact(server_unit_tests);
-    const run_helpers_unit_tests = b.addRunArtifact(helpers_unit_tests);
 
+    const run_helpers_unit_tests = b.addRunArtifact(helpers_unit_tests);
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
     test_step.dependOn(&run_helpers_unit_tests.step);
