4db8614c1846a18cdcf9f78419677523d534d154
Elyas Bianchi <elyasb99@gmail.com>
2024-11-11 15:59:55 -0800

    remove color string helper function

--- src/helpers.zig
+++ src/helpers.zig
@@ -4,22 +4,6 @@
 const std = @import("std");
 
 const Allocator = std.mem.Allocator;
-pub const Colors = enum { Green, Red };
-pub fn color_string(str: []const u8, color: Colors, allocator: Allocator) ![]const u8 {
-    switch (color) {
-        .Green => {
-            var buf = std.ArrayList(u8).init(allocator);
-            try buf.appendSlice("\x1b[32m");
-            try buf.appendSlice(str);
-            try buf.appendSlice("\x1b[0m");
-            return try buf.toOwnedSlice();
-        },
-        .Red => {
-            return "hehe";
-        },
-    }
-}
-
 // Function to convert f32 to a byte array
 pub fn f32_to_bytes(value: f32) [4]u8 {
     return std.mem.toBytes(value);
@@ -31,14 +15,6 @@ pub fn bytes_to_f32(bytes: []const u8) f32 {
 }
 
 const testing = std.testing;
-test "test color_string helper" {
-    const allocator = std.testing.allocator;
-    const str = "hello";
-    const test_colored_string = "\x1b[32mhello\x1b[0m";
-    const real_color_string = try color_string(str, Colors.Green, allocator);
-    defer allocator.free(real_color_string);
-    try testing.expectEqualSlices(u8, test_colored_string, real_color_string);
-}
 test "test type safety" {
     const val: f32 = 10.4;
     const bytes = f32_to_bytes(val);
