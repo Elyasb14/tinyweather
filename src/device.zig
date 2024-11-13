9a9ff03a965f7f30ae31d142c911f2e0bc9ecf84
Elyas Bianchi <elyasb99@gmail.com>
2024-11-12 21:07:03 -0800

    hack to make ci work if there is no serial device

--- src/device.zig
+++ src/device.zig
@@ -24,12 +24,16 @@ pub fn get_pres() [4]u8 {
 // this functions returns null when the sensor returns no data or partial data
 // the caller should return the bytearrray representing inf in f32 (std.math.inf(f32))
 pub fn parse_rain(allocator: Allocator) !?[]const f32 {
+    // TODO: this is a HACK
+    // this line is only here because sometimes we have a null pointer if there is no rain gauge device
+    _ = (std.fs.accessAbsolute("/dev/tty.usbserial-0001", .{})) catch return null;
     var buf = ArrayList(f32).init(allocator);
 
     // TODO: c.get_rain() can return a null pointer (see rg15.c get_rain())
     // This will eventually get handled when we rewrite rg15.c in zig
     // but it crashes the server because of an assert in std.mem.span
     // what to do about it now?
+
     const rain_data = std.mem.span(c.get_rain());
     if (rain_data.len < 4) return null;
 
