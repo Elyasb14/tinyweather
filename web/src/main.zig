4c2b66ede5a29644b793bb61c6f9ed620af28186
Elyas Bianchi <elyasb99@gmail.com>
2024-11-16 17:01:05 -0800

    some progress

--- web/src/main.zig
+++ web/src/main.zig
@@ -1,11 +1,21 @@
 const std = @import("std");
 const zap = @import("zap");
+const http = std.http;
 
 fn on_request(r: zap.Request) void {
-    r.setStatus(.not_found);
-    r.sendBody("<html><body><h1>404 - File not found</h1></body></html>") catch return;
-}
+    if (r.methodAsEnum() != .GET) return;
 
+    if (r.path) |path| {
+        if (std.mem.eql(u8, path, "/why")) {
+            r.setContentTypeFromFilename("why.html") catch return;
+            r.sendBody("hello") catch return;
+        } else {
+            r.setStatus(.bad_request);
+            r.setContentType(.HTML) catch return;
+            r.sendBody("<p> 400 bad request</p>");
+        }
+    }
+}
 pub fn main() !void {
     var listener = zap.HttpListener.init(.{
         .port = 3000,
