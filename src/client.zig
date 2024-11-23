ae839755f4a793e71156fbf3dbd9a4bc53a9d6fc
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2024-11-22 21:33:04 -0800

    break out getting data from node into its own function

--- src/client.zig
+++ src/client.zig
@@ -3,24 +3,7 @@ const net = std.net;
 const assert = std.debug.assert;
 const tcp = @import("lib/tcp.zig");
 
-pub fn main() !void {
-    const address = try net.Address.parseIp4("127.0.0.1", 8080);
-    const stream = net.tcpConnectToAddress(address) catch |err| {
-        std.log.err("Can't connect to address: {any}... error: {any}", .{ address, err });
-        return error.ConnectionRefused;
-    };
-    std.log.info("\x1b[32mClient initializing communication with: {any}....\x1b[0m", .{address});
-    defer stream.close();
-
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    const allocator = arena.allocator();
-
-    const sensors = &[_]tcp.SensorType{
-        tcp.SensorType.RainAcc,
-        tcp.SensorType.RainTotalAcc,
-        tcp.SensorType.RainEventAcc,
-    };
+pub fn get_data(allocator: std.mem.Allocator, sensors: []const tcp.SensorType, stream: std.net.Stream) ![]tcp.SensorData {
     const sensor_request = tcp.SensorRequest.init(sensors);
     const sensor_request_encoded = try sensor_request.encode(allocator);
     const packet = tcp.Packet.init(1, tcp.PacketType.SensorRequest, sensor_request_encoded);
@@ -36,10 +19,33 @@ pub fn main() !void {
         .SensorResponse => {
             const decoded_sensor_response = try tcp.SensorResponse.decode(sensor_request, decoded_packet.data, allocator);
             std.log.info("\x1b[32mSensor Response Packet Received\x1b[0m: {any}", .{decoded_sensor_response});
+            const sensor_data = decoded_sensor_response.data;
+            return sensor_data;
         },
         .SensorRequest => {
             std.log.err("Expected SensorResponse, got SensorRequest: {any}", .{decoded_packet});
             return tcp.TCPError.InvalidPacketType;
         },
+    }
+}
+
+pub fn main() !void {
+    const address = try net.Address.parseIp4("127.0.0.1", 8080);
+    const stream = net.tcpConnectToAddress(address) catch |err| {
+        std.log.err("Can't connect to address: {any}... error: {any}", .{ address, err });
+        return error.ConnectionRefused;
+    };
+    std.log.info("\x1b[32mClient initializing communication with: {any}....\x1b[0m", .{address});
+    defer stream.close();
+
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const sensors = [_]tcp.SensorType{ tcp.SensorType.RainTotalAcc, tcp.SensorType.Temp };
+    const sensor_data = try get_data(allocator, &sensors, stream);
+    std.debug.print("DATA RECEIVED\n\n", .{});
+    for (sensor_data) |sensor| {
+        std.debug.print("Sensor: {any}, Val: {d}\n", .{ sensor.sensor_type, sensor.val });
     }
 }
