b023aec7ff03b0436c1770b39fac1b01783eba24
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2024-11-28 19:29:54 -0800

    ProxyConnectionHandler abstraction (#49)
    * This will also move ClientHandler from lib/tcp.zig to lib/handlers.zig

--- src/node.zig
+++ src/node.zig
@@ -3,15 +3,16 @@ const net = std.net;
 const assert = std.debug.assert;
 const tcp = @import("lib/tcp.zig");
 const ArrayList = std.ArrayList;
+const handlers = @import("lib/handlers.zig");
 
 pub const std_options: std.Options = .{
     .log_level = .debug,
 };
 fn handle_client(connection: net.Server.Connection, allocator: std.mem.Allocator) !void {
-    var handler = tcp.ClientHandler.init(connection.stream);
+    var handler = handlers.ClientHandler.init(connection.stream);
     defer handler.deinit();
     while (true) {
-        try handler.handle_request(allocator) orelse break;
+        try handler.handle(allocator) orelse break;
     }
 }
 
@@ -41,80 +42,4 @@ pub fn main() !void {
         const thread = try std.Thread.spawn(.{}, handle_client, .{ connection, allocator });
         thread.detach();
     }
-}
-
-const testing = std.testing;
-
-test "Packet encoding and decoding" {
-    const allocator = testing.allocator;
-
-    const original_packet = tcp.Packet.init(1, .SensorRequest, &[_]u8{ 1, 2, 3 });
-    const encoded = try original_packet.encode(allocator);
-    defer allocator.free(encoded);
-
-    const decoded = try tcp.Packet.decode(encoded);
-
-    try testing.expectEqual(original_packet.version, decoded.version);
-    try testing.expectEqual(original_packet.type, decoded.type);
-    try testing.expectEqualSlices(u8, original_packet.data, decoded.data);
-    try testing.expectEqualDeep(original_packet, decoded);
-}
-
-test "sensor request encoding and decoding" {
-    const allocator = testing.allocator;
-
-    const original_request = tcp.SensorRequest.init(&[_]tcp.SensorType{ tcp.SensorType.Hum, tcp.SensorType.Temp });
-    const encoded_request = try original_request.encode(allocator);
-    defer allocator.free(encoded_request);
-
-    const decoded_request = try tcp.SensorRequest.decode(encoded_request, allocator);
-    defer {
-        allocator.free(decoded_request.sensors);
-    }
-
-    try testing.expectEqualSlices(tcp.SensorType, original_request.sensors, decoded_request.sensors);
-    try testing.expectEqualDeep(original_request, decoded_request);
-}
-
-test "sensor response encoding and decoding" {
-    const allocator = testing.allocator;
-    const original_request = tcp.SensorRequest.init(&[_]tcp.SensorType{ tcp.SensorType.Hum, tcp.SensorType.Temp });
-    const encoded_request = try original_request.encode(allocator);
-    defer allocator.free(encoded_request);
-
-    const decoded_request = try tcp.SensorRequest.decode(encoded_request, allocator);
-    defer allocator.free(decoded_request.sensors);
-
-    try testing.expectEqualSlices(tcp.SensorType, original_request.sensors, decoded_request.sensors);
-    try testing.expectEqualDeep(original_request, decoded_request);
-}
-
-test "end to end" {
-    const allocator = testing.allocator;
-    const sensors = &[_]tcp.SensorType{ tcp.SensorType.Gas, tcp.SensorType.Temp };
-    const sensor_request = tcp.SensorRequest.init(sensors);
-    const encoded_sensor_request = try sensor_request.encode(allocator);
-    defer allocator.free(encoded_sensor_request);
-    try testing.expectEqualSlices(u8, encoded_sensor_request, &[_]u8{ 3, 0 });
-    const packet = tcp.Packet.init(1, tcp.PacketType.SensorRequest, encoded_sensor_request);
-    const encoded_packet = try packet.encode(allocator);
-    defer allocator.free(encoded_packet);
-    const decoded_packet = try tcp.Packet.decode(encoded_packet);
-    switch (decoded_packet.type) {
-        .SensorRequest => {
-            const decoded_request_packet = try tcp.SensorRequest.decode(decoded_packet.data, allocator);
-            defer allocator.free(decoded_request_packet.sensors);
-            try testing.expectEqualDeep(decoded_request_packet, sensor_request);
-            const response_packet = tcp.SensorResponse.init(decoded_request_packet, undefined);
-            const encoded_response_packet = try response_packet.encode(allocator);
-            defer allocator.free(encoded_response_packet);
-            const new_packet = tcp.Packet.init(1, tcp.PacketType.SensorResponse, encoded_response_packet);
-            const new_packet_encoded = try new_packet.encode(allocator);
-            defer allocator.free(new_packet_encoded);
-            const new_packet_decoded = tcp.Packet.decode(new_packet_encoded);
-            try testing.expectEqualDeep(new_packet, new_packet_decoded);
-        },
-        else => {},
-    }
-    try testing.expectEqualDeep(packet, decoded_packet);
 }
