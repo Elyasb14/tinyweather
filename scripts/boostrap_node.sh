bd6ea792a0ea667917607e4fe3529125627109a7
Elyas <elyasb99@gmail.com>
2024-12-08 23:59:03 +0000

    bug when someone sends packet with wrong version number

--- scripts/boostrap_node.sh
+++ scripts/boostrap_node.sh
@@ -11,3 +11,24 @@ fi
 
 echo -e "\x1b[32mMoving executable ./zig-out/bin/tinyweather-node to /opt/tinyweather/tinyweather-node\x1b[0m"
 cp ./zig-out/bin/tinyweather-node /opt/tinyweather
+
+if [[ ! -f /etc/systemd/system/tinyweather-node.service ]]; then 
+    touch /etc/systemd/system/tinyweather-node.service
+else
+    rm /etc/systemd/system/tinyweather-node.service
+fi
+
+sudo echo "
+[Unit]
+Description=Tinyweather Node Service
+After=network.target
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=5s
+ExecStart=/opt/tinyweather/tinyweather-node
+WorkingDirectory=/opt/tinyweather
+
+[Install]
+WantedBy=multi-user.target" >> /etc/systemd/system/tinyweather-node.service
