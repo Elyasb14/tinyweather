eb77f29c137e9ac6dedece71804ec299f5ab37ef
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2024-12-08 13:14:56 -0800

    moving to linux

--- scripts/boostrap_node.py
+++ scripts/boostrap_node.py
@@ -1,4 +1,7 @@
-
+import subprocess
 
 if __name__ == "__main__":
-    print("hellow, world")
+
+    print("\x1b[32mBeginning the bootstrapping of tinyweather-node\x1b[0m")
+    subprocess.run(["zig", "build"])
+    print("\x1b[32mBuilt tinyweather-node, executable in ./zig-out/bin/tinyweather-node\x1b[0m")
