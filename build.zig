31845cf84aa843d35cd573dfad0a789e650b2d80
Elyas <elyasb99@gmail.com>
2024-12-08 23:38:43 +0000

    progress

--- build.zig
+++ build.zig
@@ -57,6 +57,7 @@ pub fn build(b: *std.Build) void {
     run_all_step.dependOn(&run_proxy.step);
     run_all_step.dependOn(&run_web.step);
 
+
     const server_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/lib/tcp.zig"),
         .target = target,
--- scripts/boostrap_node.sh
+++ scripts/boostrap_node.sh
@@ -1,5 +1,13 @@
-#! /bin/bash
+#!/bin/bash
 
-echo "\x1b[32mBeginning the bootstrapping of tinyweather-node\x1b[0m"
+echo -e "\x1b[32mBeginning the bootstrapping of tinyweather-node\x1b[0m"
 zig build
-echo "\x1b[32mBuilt tinyweather-node, executable in ./zig-out/bin/tinyweather-node\x1b[0m"
+echo -e "\x1b[32mBuilt tinyweather-node, executable in ./zig-out/bin/tinyweather-node\x1b[0m"
+
+if [[ ! -d "/opt/tinyweather" ]]; then
+    echo -e "\x1b[32mCreating directory:\x1b[0m \x1b[35m/opt/tinyweather\x1b[0m"
+    mkdir /opt/tinyweather
+fi
+
+echo -e "\x1b[32mMoving executable ./zig-out/bin/tinyweather-node to /opt/tinyweather/tinyweather-node\x1b[0m"
+cp ./zig-out/bin/tinyweather-node /opt/tinyweather
