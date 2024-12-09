e09ff2ebe3d2bf93b9662e740b31be6cb455a3de
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2024-12-09 07:29:19 -0800

    move args struct to a file

--- src/node.zig
+++ src/node.zig
@@ -4,80 +4,10 @@ const assert = std.debug.assert;
 const tcp = @import("lib/tcp.zig");
 const ArrayList = std.ArrayList;
 const handlers = @import("lib/handlers.zig");
+const Args = @import("lib/Args.zig");
 
 pub const std_options: std.Options = .{
     .log_level = .debug,
-};
-
-fn help() noreturn {
-    std.debug.print(
-        \\Usage: 
-        \\  ./tinyweather-node --address [ip_address] --port [port]
-        \\
-        \\Options:
-        \\  ip_address (optional)  The IP address to bind to (default: 127.0.0.1)
-        \\  port (optional)        The port to listen on (default: 8080)
-        \\Example:
-        \\  ./tinyweather-node 10.0.0.7 9090
-        \\
-    , .{});
-    std.process.exit(1);
-}
-
-const Args = struct {
-    port: u16,
-    address: []const u8,
-    it: std.process.ArgIterator,
-
-    const Option = enum {
-        @"--address",
-        @"--port",
-        @"--help",
-    };
-
-    pub fn deinit(self: *Args) void {
-        self.it.deinit();
-    }
-
-    pub fn parse(allocator: std.mem.Allocator) !Args {
-        var args = try std.process.argsWithAllocator(allocator);
-        _ = args.next() orelse "tinyweather-node";
-
-        var port: u16 = 8080;
-        var address: []const u8 = "127.0.0.1";
-
-        while (args.next()) |arg| {
-            const option = std.meta.stringToEnum(Option, arg) orelse {
-                std.debug.print("{s} is not a valid argument\n", .{arg});
-                help();
-            };
-
-            switch (option) {
-                .@"--address" => {
-                    address = args.next() orelse {
-                        std.debug.print("--address provided with no argument\n", .{});
-                        help();
-                    };
-                },
-                .@"--port" => {
-                    const port_s = args.next() orelse {
-                        std.debug.print("--port provided with no argument\n", .{});
-                        help();
-                    };
-                    port = std.fmt.parseInt(u16, port_s, 10) catch {
-                        std.debug.print("--port argument is not a valid u16\n", .{});
-                        help();
-                    };
-                },
-                .@"--help" => help(),
-            }
-        }
-        return .{
-            .address = address,
-            .port = port,
-            .it = args,
-        };
-    }
 };
 
 fn handle_client(connection: net.Server.Connection, allocator: std.mem.Allocator) !void {
