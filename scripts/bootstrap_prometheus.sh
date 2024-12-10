fe1a06763d616f80d26cf2519ec9333996ad2a1b
Elyas Bianchi <elyasb99@gmail.com>
2024-12-09 21:42:04 -0800

    blah

--- scripts/bootstrap_prometheus.sh
+++ scripts/bootstrap_prometheus.sh
@@ -1,3 +1,13 @@
 #! /bin/bash
 
 echo -e "\x1b[33mBootstrapping Prometheus on system"
+
+
+# WARNING: this needs npm (sigh) 
+# Ensure you have Go installed for ARM64
+# Clone Prometheus
+git clone https://github.com/prometheus/prometheus.git
+cd prometheus
+
+# Build specifically for ARM64
+GOOS=linux GOARCH=arm64 make build
