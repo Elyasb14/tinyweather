2fc213c03ff9e2945ffc8a3245731e2b41e170de
Elyas <elyasb99@gmail.com>
2024-12-27 03:57:36 +0000

    stuff

--- src/lib/sensors/bme680/bme680.zig
+++ src/lib/sensors/bme680/bme680.zig
@@ -9,7 +9,7 @@ const c = @cImport({
 const i2c_device = "/dev/i2c-1";
 
 const Device = struct {
-    fd: std.fs.File.Handle,
+    fd: std.fs.File,
 
     pub fn init(path: []const u8) !Device {
         const fd = try std.fs.openFileAbsolute(path, .{ .mode = .read_write });
@@ -19,21 +19,23 @@ const Device = struct {
             std.debug.print("ioctl failed, errno\n", .{});
         }
 
-        return .{ .fd = fd.handle };
+        return .{ .fd = fd };
     }
 
-    fn write() void {}
-    
-fn read() void {}
+    fn write(self: *Device) void {
+        var buf: [32]u8 = undefined;
+        const ret_code = self.fd.write(&buf);
+        std.debug.print("{any}\n", .{ret_code});
+    }
+    fn read() void {}
 
-    pub fn set_registers(register_addr: c_int, register_data: c_int, len: u32, device: *Device) u8 {
-        
-}
+    // pub fn set_registers(register_addr: c_int, register_data: c_int, len: u32, device: *Device) u8 {
+
 };
 
 pub fn main() !void {
     const device = try Device.init(i2c_device);
-    const register_buf: [32]u8 = undefined;
+    device.write();
 
-    std.debug.print("{any}{any}\n", .{ device, register_buf });
+    std.debug.print("{any}\n", .{device});
 }
