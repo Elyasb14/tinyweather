da0cfb0d508857463c6afcdcc7560b16943aa925
Elyas Bianchi <elyasb99@gmail.com>
2024-12-27 22:30:24 +0000

    progress

--- src/lib/device.zig
+++ src/lib/device.zig
@@ -4,29 +4,50 @@ const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
 const tcp = @import("tcp.zig");
 const builtin = @import("builtin");
+const bme = @import("sensors/bme680/bme680.zig");
 const c = @cImport({
     @cInclude("lib/sensors/rg15/rg15.h");
 });
 
-pub fn get_gas() [4]u8 {
-    const random_gas = std.crypto.random.float(f32) * 200.0;
-    return helpers.f32_to_bytes(random_gas);
+// pub fn get_gas(allocator: Allocator) ![4]u8 {
+//     const random_gas = std.crypto.random.float(f32) * 200.0;
+//     return helpers.f32_to_bytes(random_gas);
+// }
+//
+// pub fn get_temp() [4]u8 {
+//     const random_temp = std.crypto.random.float(f32) * 50.0 - 20.0;
+//     return helpers.f32_to_bytes(random_temp);
+// }
+//
+// pub fn get_hum() [4]u8 {
+//     const random_hum = std.crypto.random.float(f32) * 100.0;
+//     return helpers.f32_to_bytes(random_hum);
+// }
+//
+// pub fn get_pres() [4]u8 {
+//     const random_pres = std.crypto.random.float(f32) * 500.0 + 500.0;
+//     return helpers.f32_to_bytes(random_pres);
+// }
+//
+pub fn parse_bme(allocator: Allocator) !?[]const f32 {
+    var buf = ArrayList(f32).init(allocator);
+
+    const bme_data = try bme.exec_python(allocator) orelse return null;
+    std.debug.print("{s}\n", .{bme_data});
+    var split = std.mem.splitAny(u8, bme_data, " ");
+    while (split.next()) |token| {
+        const val = std.fmt.parseFloat(f32, token) catch {
+            const temp = std.fmt.parseInt(u64, token, 10) catch continue;
+            try buf.append(@floatFromInt(temp));
+            continue;
+        };
+        try buf.append(val);
+    }
+    const data = try buf.toOwnedSlice();
+    std.debug.print("{any}\n", .{data});
+    return data;
 }
 
-pub fn get_temp() [4]u8 {
-    const random_temp = std.crypto.random.float(f32) * 50.0 - 20.0;
-    return helpers.f32_to_bytes(random_temp);
-}
-
-pub fn get_hum() [4]u8 {
-    const random_hum = std.crypto.random.float(f32) * 100.0;
-    return helpers.f32_to_bytes(random_hum);
-}
-
-pub fn get_pres() [4]u8 {
-    const random_pres = std.crypto.random.float(f32) * 500.0 + 500.0;
-    return helpers.f32_to_bytes(random_pres);
-}
 /// this functions returns null when the sensor returns no data or partial data
 /// in the case we do return null, the caller should "orelse" the bytearrray representing nan in f32 (std.math.nan(f32))
 pub fn parse_rain(allocator: Allocator) !?[]const f32 {
--- src/lib/tcp.zig
+++ src/lib/tcp.zig
@@ -90,14 +90,15 @@ pub const SensorResponse = struct {
 
         // Existing rain data parsing
         const rain_data: []const f32 = (try device.parse_rain(allocator)) orelse &[_]f32{std.math.nan(f32)} ** 4;
+        const bme_data: []const f32 = (try device.parse_bme(allocator)) orelse &[_]f32{std.math.nan(f32)} ** 4;
 
         var buf = ArrayList(u8).init(allocator);
         for (self.request.sensors) |sensor| {
             switch (sensor) {
-                .Gas => try buf.appendSlice(&device.get_gas()),
-                .Temp => try buf.appendSlice(&device.get_temp()),
-                .Pres => try buf.appendSlice(&device.get_pres()),
-                .Hum => try buf.appendSlice(&device.get_hum()),
+                .Gas => try buf.appendSlice(&helpers.f32_to_bytes(bme_data[3])),
+                .Temp => try buf.appendSlice(&helpers.f32_to_bytes(bme_data[0])),
+                .Pres => try buf.appendSlice(&helpers.f32_to_bytes(bme_data[1])),
+                .Hum => try buf.appendSlice(&helpers.f32_to_bytes(bme_data[2])),
                 .RainAcc => try buf.appendSlice(&helpers.f32_to_bytes(rain_data[0])),
                 .RainEventAcc => try buf.appendSlice(&helpers.f32_to_bytes(rain_data[1])),
                 .RainTotalAcc => try buf.appendSlice(&helpers.f32_to_bytes(rain_data[2])),
