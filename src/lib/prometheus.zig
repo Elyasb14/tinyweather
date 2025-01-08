822b215084cf6cd063108302efc159c259cb4ec4
Elyas Bianchi <elyasb99@gmail.com>
2025-01-08 11:59:05 -0800

    rework of to_prometheus

--- src/lib/prometheus.zig
+++ src/lib/prometheus.zig
@@ -17,8 +17,27 @@ pub const Gauge = struct {
         self.val = val;
     }
 
-    pub fn to_prometheus(self: *Gauge, allocator: std.mem.Allocator) ![]const u8 {
-        // TODO: remove allocPrint here and replace with ArrayList
-        return std.fmt.allocPrint(allocator, "# HELP {s} {s}\n# TYPE {s} gauge\n{s} {d}\n", .{ self.name, self.help_text, self.name, self.name, self.val });
+    pub fn to_prometheus(self: *const Gauge, allocator: std.mem.Allocator) ![]const u8 {
+        var result = std.ArrayList(u8).init(allocator);
+        errdefer result.deinit();
+
+        // Write HELP metadata
+        try result.writer().print("# HELP {s} {s}\n", .{
+            self.name,
+            self.help_text,
+        });
+
+        // Write TYPE metadata
+        try result.writer().print("# TYPE {s} gauge\n", .{
+            self.name,
+        });
+
+        // Write the actual gauge value
+        try result.writer().print("{s} {d}\n", .{
+            self.name,
+            self.val,
+        });
+
+        return result.toOwnedSlice();
     }
 };
