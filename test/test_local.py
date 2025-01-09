3b8201dec1bde3349fc6356389bd0163439f5e3c
Elyas Bianchi <elyasb99@gmail.com>
2025-01-09 04:44:11 +0000

    some fixes

--- test/test_local.py
+++ test/test_local.py
@@ -4,22 +4,26 @@ import time
 
 
 if __name__ == "__main__":
+    subprocess.run("source .venv/bin/activate", shell=True, executable="/bin/bash")
     result = subprocess.run(["zig", "build"], check=True)
     assert result != 0, "zig build failed"
     time.sleep(1)
+    print("\x1b[32mzig build ran\x1b[0m")
 
     subprocess.Popen(["./zig-out/bin/tinyweather-node"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     time.sleep(1)
+    print("\x1b[32mnode started...\x1b[0m")
 
     subprocess.Popen(["./zig-out/bin/tinyweather-proxy"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     time.sleep(1)
+    print("\x1b[32mproxy started...\x1b[0m")
 
     subprocess.run([
         'curl',
-        '--parallel',
-        'localhost:8081/metrics',
-        'localhost:8081/metrics',
-        'localhost:8081/metrics',
+        # '--parallel',
+        # 'localhost:8081/metrics',
+        # 'localhost:8081/metrics',
+        # 'localhost:8081/metrics',
         'localhost:8081/metrics',
         '-H', 'sensor:Temp',
         '-H', 'sensor:RainTotalAcc'
