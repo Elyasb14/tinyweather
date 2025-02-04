0ec2206915ea36da3a04841d97c007866570eaf6
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2025-02-04 15:52:32 -0800

    grafana bootstrap script (#94)

---
+++ scripts/bootstrap_grafana.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+ARCH=$(uname -m)
+
+if [ "$ARCH" = "x86_64" ]; then
+    ARCH="amd64"
+fi
+
+if [[ -d /opt/grafana ]]; then 
+  rm -rf /opt/grafana
+fi
+
+wget --quiet https://dl.grafana.com/oss/release/grafana-11.5.1.linux-${ARCH}.tar.gz
+tar -zxvf grafana-11.5.1.linux-${ARCH}.tar.gz
+
+rm -rf grafana-11.5.1.linux-${ARCH}.tar.gz
+
+mv grafana* /opt/grafana
+
+if [[ -f /etc/systemd/system/grafana.service ]]; then 
+  rm /etc/systemd/system/grafana.service
+fi
+
+touch /etc/systemd/system/grafana.service
+
+echo "
+[Unit]
+Description=Grafana
+After=network.target
+[Service]
+Type=simple
+Restart=always
+RestartSec=5s
+ExecStart=/opt/grafana/bin/grafana-server
+WorkingDirectory=/opt/grafana
+[Install]
+WantedBy=multi-user.target" >> /etc/systemd/system/grafana.service
+
+systemctl daemon-reload
+systemctl start grafana.service
+systemctl enable grafana.service
+systemctl status grafana.service
--- scripts/bootstrap_prometheus.sh
+++ scripts/bootstrap_prometheus.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
-# Check if the web listen address argument is provided
+ARCH=$(uname -m)
+
+if [ "$ARCH" = "x86_64" ]; then
+    ARCH="amd64"
+fi
+
 if [ $# -ne 1 ]; then
     echo -e "\x1b[31mError: Please provide the web listen address.\x1b[0m"
     echo -e "\x1b[33mUsage: $0 <web-listen-address>\x1b[0m"
@@ -8,7 +13,6 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-# Store command line argument
 WEB_LISTEN_ADDRESS=$1
 
 if [[ -f /etc/systemd/system/prometheus.service ]]; then 
@@ -17,16 +21,12 @@ if [[ -f /etc/systemd/system/prometheus.service ]]; then
     rm /etc/systemd/system/prometheus.service
     systemctl daemon-reload
     echo -e "\x1b[32mSystemd daemon reloaded successfully.\x1b[0m"
-else
-    echo -e "\x1b[32mNo existing prometheus service file found. Skipping removal steps.\x1b[0m"
 fi
 
-echo -e "\x1b[33mBootstrapping Prometheus on system"
-echo -e "\x1b[33mGet and build prometheus\x1b[0m"
-wget --quiet https://github.com/prometheus/prometheus/releases/download/v3.1.0/prometheus-3.1.0.linux-amd64.tar.gz
-tar xf prometheus-3.1.0.linux-amd64.tar.gz
+wget --quiet https://github.com/prometheus/prometheus/releases/download/v3.1.0/prometheus-3.1.0.linux-${ARCH}.tar.gz
+tar xf prometheus-3.1.0.linux-${ARCH}.tar.gz
 touch prometheus.yml
-# creates a prometheus config
+
 echo "global:
   scrape_interval: 15s
 scrape_configs:
@@ -39,19 +39,14 @@ scrape_configs:
       Port: 
         values: [\"8080\"]
       Sensor:
-        values: [\"Temp\", \"RainTotalAcc\"]" >> ./prometheus.yml
+        values: [\"Temp\", \"RainTotalAcc\", \"Hum\", \"Pres\", \"Gas\"]" >> ./prometheus.yml
 
-# checks that the config is valid
-./prometheus-3.1.0.linux-amd64/promtool check config prometheus.yml
+./prometheus-3.1.0.linux-${ARCH}/promtool check config prometheus.yml
 rm -rf /opt/prometheus
-echo -e "\x1b[33mPreparing /opt/prometheus directory...\x1b[0m"
 mkdir -p /opt/prometheus
 mv ./prometheus.yml /opt/prometheus/prometheus.yml
-echo -e "\x1b[33mMoving newly built prometheus executable to /opt/prometheus\x1b[0m"
-mv ./prometheus-3.1.0.linux-amd64/prometheus /opt/prometheus/prometheus
-echo -e "\x1b[33mCreating systemd service file at /etc/systemd/system/prometheus.service...\x1b[0m"
+mv ./prometheus-3.1.0.linux-${ARCH}/prometheus /opt/prometheus/prometheus
 touch /etc/systemd/system/prometheus.service
-echo -e "\x1b[33mWriting service configuration to /etc/systemd/system/prometheus.service...\x1b[0m"
 echo "
 [Unit]
 Description=prometheus Node Service
@@ -64,14 +59,10 @@ ExecStart=/opt/prometheus/prometheus --web.listen-address=\"$WEB_LISTEN_ADDRESS\
 WorkingDirectory=/opt/prometheus
 [Install]
 WantedBy=multi-user.target" >> /etc/systemd/system/prometheus.service
-echo -e "\x1b[32mService configuration written to /etc/systemd/system/prometheus.service\x1b[0m"
-echo -e "\x1b[33mReloading systemd daemon to recognize the new prometheus service...\x1b[0m"
+
 systemctl daemon-reload
-echo -e "\x1b[32mSystemd daemon reloaded successfully.\x1b[0m"
-echo -e "\x1b[33mStarting the prometheus service...\x1b[0m"
 systemctl start prometheus
 systemctl enable prometheus
-echo -e "\x1b[32mprometheus service started.\x1b[0m"
-mv ./prometheus-3.1.0.linux-amd64/promtool /opt/prometheus/promtool
+mv ./prometheus-3.1.0.linux-${ARCH}/promtool /opt/prometheus/promtool
 rm -rf prometheus*
 systemctl status prometheus
