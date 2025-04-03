f3c51b6adef8aff6b7a002441e57509df3dae05e
Elyas <elyasb99@gmail.com>
2025-04-02 21:40:32 -0700

    testing i2c stuff

---
+++ src/lib/sensors/test/test.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
+
+#define I2C_DEVICE "/dev/i2c-1"  // Change this to the appropriate I2C bus on your system
+#define SENSOR_ADDR 0x40         // The I2C address of your sensor
+
+int i2c_file;
+
+// Function to read registers from the I2C device
+int read_sensor_reg(int file, int reg, unsigned char *buffer, size_t length) {
+    if (buffer == NULL) {
+        printf("Buffer ERROR!! : null pointer\n");
+        return -1;
+    }
+    
+    // Write the register address we want to read from
+    if (write(file, &reg, 1) != 1) {
+        printf("Failed to write register address\n");
+        return -1;
+    }
+    
+    // Wait a bit for the sensor to process
+    usleep(20000);  // 20ms delay
+    
+    // Read the data from the specified register
+    if (read(file, buffer, length) != length) {
+        printf("Failed to read from device\n");
+        return -1;
+    }
+    
+    return length;
+}
+
+int main() {
+    unsigned char buf[4];
+    uint data, data1;
+    float temp, hum;
+    
+    // Open the I2C device
+    if ((i2c_file = open(I2C_DEVICE, O_RDWR)) < 0) {
+        printf("Failed to open the I2C bus\n");
+        exit(1);
+    }
+    
+    // Set the I2C slave address for all subsequent I/O
+    if (ioctl(i2c_file, I2C_SLAVE, SENSOR_ADDR) < 0) {
+        printf("Failed to acquire bus access and/or talk to slave\n");
+        exit(1);
+    }
+    
+    while (1) {
+        // Read temperature and humidity registers
+        if (read_sensor_reg(i2c_file, 0x00, buf, 4) < 0) {
+            printf("Error reading sensor data\n");
+        } else {
+            // Calculate temperature and humidity like in the Arduino code
+            data = (buf[0] << 8) | buf[1];
+            data1 = (buf[2] << 8) | buf[3];
+            
+            temp = ((float)data * 165.0 / 65535.0) - 40.0;
+            hum = ((float)data1 / 65535.0) * 100.0;
+            
+            printf("temp(C): %.2f\thum(%%RH): %.2f\n", temp, hum);
+        }
+        
+        usleep(500000);  // 500ms delay
+    }
+    
+    close(i2c_file);
+    return 0;
+}
